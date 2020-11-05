f9947e74a2808c08984d134471f53b5035be7b29
dogezhou <doge.zhou@outlook.com>
2020-11-05 16:06:33 +0800

    feat(runtime): add dependency tracking

--- src/runtime-core.ts
+++ src/runtime-core.ts
@@ -3,21 +3,57 @@
  */
 import { debounce } from "./utils";
 
-const watchers: Array<() => any> = []
+// 依赖收集器
+const dependencyTracker = new Set<symbol>()
+
+/**
+ * watchers 需要添加 dependencies 来记录他的依赖，
+ * 依赖指 reactive 中的属性
+ */
+const watchers: Array<{
+    callback: () => any,
+    dependencies: Set<symbol>
+}> = []
+
 const watch = (callback: () => any) => {
+    // 1. 先清空依赖收集器
+    dependencyTracker.clear()
+    // 2. 运行一次 watch callback，触发 reactive getter，在 getter 中填充依赖收集器
     callback()  // In Vue 3.0, watchers are fired immediately after component mount.
-    watchers.push(debounce(callback, 0))
+    watchers.push({
+        callback: debounce(callback, 0),
+        // 4. 保存 watcher 的依赖
+        dependencies: new Set(dependencyTracker)
+    })
 }
 
 // 添加泛型（extends object 约束 T 是 object)，使用时也可以不手动指定，让类型推导自动推算
 const reactive = <T extends object>(target: T): T => {
+    // 记录 key Symbol(key) 的 Map
+    const keyToSymbolMap = new Map<keyof T, symbol>()
+    const getSymbolForKey = (key: keyof T): symbol => {
+        const symbol = keyToSymbolMap.get(key) || Symbol()
+        if (!keyToSymbolMap.has(key)) {
+            keyToSymbolMap.set(key, symbol)
+        }
+        return symbol
+    }
     return new Proxy(target, {
         get(target, key: keyof T) {
+            // 3. 在 getter 中填充依赖收集器
+            dependencyTracker.add(getSymbolForKey(key))
             return target[key]
         },
         set(target, key: keyof T, value) {
             target[key] = value
-            watchers.forEach(watcher => watcher())
+
+            // 5. 根据依赖触发 watcher
+            watchers.filter(({ dependencies }) => {
+                return dependencies.has(getSymbolForKey(key))
+            }).forEach(({ callback }) => {
+                callback()
+            })
+
             return true
         },
     })
